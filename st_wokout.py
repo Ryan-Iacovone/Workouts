6940ee89be5017c05aa3d7b61f44a248412cb67b
ryanike11 <ryanike11@icloud.com>
2024-11-04 10:50:30 -0500

    update to public

--- st_wokout.py
+++ st_wokout.py
@@ -1,60 +1,13 @@
 import os
 import pandas as pd
 import streamlit as st
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+from streamlit_gsheets import GSheetsConnection
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
 
-# The ID and range of a sample spreadsheet.
-SPREADSHEET_ID = st.secrets["sheet_id"]
-
-def read_in_google_sheet():
-  """Shows basic usage of the Sheets API.
-  Prints values from a sample spreadsheet.
-  """
-  creds = st.secrets["token"]
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
-  """if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
-"""
-  try:
-    service = build("sheets", "v4", credentials=creds)
-
-    # Call the Sheets API
-    sheet = service.spreadsheets()
-    result = (
-        sheet.values()
-        .get(spreadsheetId=SPREADSHEET_ID, range="Form Responses 1")
-        .execute()
-    )
-    values = result.get("values", [])
-
-    df = pd.DataFrame(values[1:], columns=values[0], index=None)
-    return df
-
-  except HttpError as err:
-    print(err)
-
-workout = read_in_google_sheet()
+# Read in the public google sheet file
+workout = conn.read()
 
 #Catching any extra spaces when I typed in workouts 
 workout["Exercise"] = workout["Exercise"].str.strip()
